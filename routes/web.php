ad262c3f8d1ee89f460254427bf3b6939042a1bd
AlifiyulAkyun <73621480+AlifiyulAkyun@users.noreply.github.com>
2022-02-21 16:25:59 +0700

    Kode Program yang diubah

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/',function(){
+    return "Selamat datang";
+});
+Route::get('/about',function(){
+    return "2041720036,Alifiyul Akyun,TI-2B";
+});
+Route::get('/articles/{id}',function($id=null){
+    return "Ini adalah halaman artikel dengan id = " .$id;
+});
